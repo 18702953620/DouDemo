b2ee48f7e34e6e9a767d2227921b2248a3e03180
h.cheng <18702953620@163.com>
2018-11-30 10:16:06 +0800

    添加翻页时，加载更多逻辑

--- app/src/main/java/com/ch/doudemo/activity/PageActivity.java
+++ app/src/main/java/com/ch/doudemo/activity/PageActivity.java
@@ -1,11 +1,16 @@
 package com.ch.doudemo.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.ch.doudemo.R;
-import com.ch.doudemo.widget.VerticalViewPager;
 import com.ch.doudemo.adapter.VerticalViewPagerAdapter;
+import com.ch.doudemo.widget.VerticalViewPager;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +25,10 @@ public class PageActivity extends AppCompatActivity {
 
     @BindView(R.id.vvp_back_play)
     VerticalViewPager vvpBackPlay;
+    @BindView(R.id.srl_page)
+    SmartRefreshLayout srlPage;
     private List<String> urlList;
+    private VerticalViewPagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +40,61 @@ public class PageActivity extends AppCompatActivity {
     }
 
     private void addListener() {
+        srlPage.setEnableAutoLoadMore(false);
+        srlPage.setEnableLoadMore(false);
+        srlPage.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                srlPage.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        urlList.addAll(urlList);
+                        pagerAdapter.setUrlList(urlList);
+                        pagerAdapter.notifyDataSetChanged();
 
+                        srlPage.finishLoadMore();
+                    }
+                }, 2000);
+
+            }
+
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+
+            }
+        });
     }
 
     private void initView() {
         makeData();
-        VerticalViewPagerAdapter pagerAdapter = new VerticalViewPagerAdapter(getSupportFragmentManager());
+        pagerAdapter = new VerticalViewPagerAdapter(getSupportFragmentManager());
         vvpBackPlay.setVertical(true);
         vvpBackPlay.setOffscreenPageLimit(10);
         pagerAdapter.setUrlList(urlList);
         vvpBackPlay.setAdapter(pagerAdapter);
+        vvpBackPlay.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == urlList.size() - 1) {
+                    srlPage.setEnableAutoLoadMore(true);
+                    srlPage.setEnableLoadMore(true);
+                } else {
+                    srlPage.setEnableAutoLoadMore(false);
+                    srlPage.setEnableLoadMore(false);
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
 
     }
