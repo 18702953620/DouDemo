a639ebe305431f0a47c9b44dd9688e4ec8031fb5
18702953620@163.com <chenghuan3824>
2019-10-12 18:56:10 +0800

    替换原先的ViewPager

--- app/src/main/java/com/ch/doudemo/activity/PageActivity.java
+++ app/src/main/java/com/ch/doudemo/activity/PageActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.ch.doudemo.R;
 import com.ch.doudemo.adapter.VerticalViewPagerAdapter;
 import com.ch.doudemo.widget.VerticalViewPager;
+import com.ch.doudemo.widget.VerticalViewPager2;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
@@ -24,7 +25,7 @@ import butterknife.ButterKnife;
 public class PageActivity extends AppCompatActivity {
 
     @BindView(R.id.vvp_back_play)
-    VerticalViewPager vvpBackPlay;
+    VerticalViewPager2 vvpBackPlay;
     @BindView(R.id.srl_page)
     SmartRefreshLayout srlPage;
     private List<String> urlList;
@@ -68,11 +69,12 @@ public class PageActivity extends AppCompatActivity {
     private void initView() {
         makeData();
         pagerAdapter = new VerticalViewPagerAdapter(getSupportFragmentManager());
-        vvpBackPlay.setVertical(true);
+//        vvpBackPlay.setVertical(true);
         vvpBackPlay.setOffscreenPageLimit(10);
         pagerAdapter.setUrlList(urlList);
         vvpBackPlay.setAdapter(pagerAdapter);
-        vvpBackPlay.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+        vvpBackPlay.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -87,7 +89,6 @@ public class PageActivity extends AppCompatActivity {
                     srlPage.setEnableAutoLoadMore(false);
                     srlPage.setEnableLoadMore(false);
                 }
-
             }
 
             @Override
@@ -95,8 +96,6 @@ public class PageActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
 
     private void makeData() {
